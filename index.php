bdbaa1a701355dc23b514976082fd05de2454a21
unknown <patjamessliney@gmail.com>
2019-07-01 18:53:13 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Noodle</title>
+    <link rel="stylesheet" type="text/css" href="assets/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet"> 
+</head>
+<body>
+    <div class="wrapper indexPage">
+
+    <div class="mainSection">
+
+        <div class="logoContainer">
+            <img src="assets/img/logo/noodlepng.png" alt="Noodle Logo">
+        </div>
+
+
+        <div class="searchContainer">
+            <form action="search.php" method="GET">
+
+                <input class="searchBox" type="text" name="term" placeholder="Feeling Hungry?">
+
+                <input class="searchButton" type="submit" value="Search">
+
+            </form>
+        </div>
+
+    </div>
+
+    </div>
+</body>
+</html>
